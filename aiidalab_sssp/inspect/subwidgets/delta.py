34511e5d9f523de13d15209c89c37615be6282cc
Jusong Yu <jusong.yeu@gmail.com>
2023-03-24 23:04:33 +0000

    EOS plot in one figure

--- aiidalab_sssp/inspect/subwidgets/delta.py
+++ aiidalab_sssp/inspect/subwidgets/delta.py
@@ -1,5 +1,5 @@
 """Moudle contains widgets for accuracy delat results inspect.
-The widget EosWidget for showing Eos fit line of a given pseudo in the given configuration.
+The widget EosComparisonWidget for compare Eos fit line of a given pseudos in the given configuration.
 The widget AccuracyMeritWidget showing Nicola's Nu measure of all pseudos in all configurations"""
 import ipywidgets as ipw
 import matplotlib.pyplot as plt
@@ -64,9 +64,8 @@ class AccuracyMeritWidget(ipw.VBox):
 
     @staticmethod
     def _render_plot(pseudos: dict, measure_type):
-
-        px = 1 / plt.rcParams["figure.dpi"]  # pixel in inches
-        fig, ax = plt.subplots(1, 1, figsize=(1024 * px, 360 * px))
+        """Render the plot for the given pseudos and measure type."""
+        fig, ax = plt.subplots(1, 1, figsize=(1024 * _px, 360 * _px))
         # conf_list store configuration list of every pseudo
         conf_list = {}
         for label, data in pseudos.items():
@@ -132,123 +131,166 @@ class AccuracyMeritWidget(ipw.VBox):
         return fig
 
 
-class EosWidget(ipw.VBox):
+class EosComparisonWidget(ipw.VBox):
+    """This widget is used to compare the equation of state of two different
+    pseudopotentials.
+    Two subplots are shown for two different pseudopotentials. The drowdown menu
+    allows user to select the pseudopotential and configuration.
+    """
 
     pseudos = traitlets.Dict(allow_none=True)
 
     def __init__(self):
-        self.select_pseudo = ipw.Dropdown()
-        self.select_pseudo.observe(self._on_pseudo_select, names="value")
+        self.select_pseudo_ref = ipw.Dropdown()
+        self.select_pseudo_ref.observe(self._on_pseudo_select, names="value")
+
+        self.select_pseudo_comp = ipw.Dropdown()
+        self.select_pseudo_comp.observe(self._on_pseudo_select, names="value")
 
         self.select_configuration = ipw.Dropdown()
-        self.select_configuration.observe(self._on_configuration_selecet, names="value")
+        self.select_configuration.observe(self._on_configuration_change, names="value")
 
         self.eos_preview = ipw.Output()  # empty plot with a instruction ask for select
 
         super().__init__(
             children=[
-                # ipw.HTML("<h2> Equation of State Pseudopotential w.r.t AE </h2>"),
-                ipw.HBox(children=[self.select_pseudo, self.select_configuration]),
+                ipw.HBox(
+                    children=[
+                        self.select_pseudo_ref,
+                        self.select_pseudo_comp,
+                    ]
+                ),
+                self.select_configuration,
                 self.eos_preview,
             ],
         )
 
     @traitlets.observe("pseudos")
     def _on_pseudos_change(self, change):
-        if change["new"]:
-            self.layout.visibility = "visible"
-            self.select_pseudo.options = list(self.pseudos.keys())
+        if change["new"] is not None and len(change["new"]) > 0:
+            self.layout.display = "block"
+            with self.hold_trait_notifications():
+                pseudo_list = list(self.pseudos.keys())
+                self.select_pseudo_ref.options = pseudo_list
+                self.select_pseudo_comp.options = pseudo_list
+
+            self.update_plot()
         else:
-            self.layout.visibility = "hidden"
+            self.layout.display = "none"
 
-    def _on_pseudo_select(self, change):
-        """Update configuration dropdown list and select first entity"""
-        if change["new"]:
-            label = change["new"]
-            _data = self.pseudos[label]["accuracy"]["delta"]
+    def _on_pseudo_select(self, _):
+        """Update configuration dropdown options"""
+        label_ref = self.select_pseudo_ref.value
+        label_comp = self.select_pseudo_comp.value
 
-            configuration_list = [i for i in CONFIGURATIONS if i in _data.keys()]
-            self.select_configuration.options = configuration_list
+        if label_ref is None or label_comp is None:
+            return
 
-            self._render()
+        _data_ref = self.pseudos[label_ref]["accuracy"]["delta"]
+        _data_comp = self.pseudos[label_comp]["accuracy"]["delta"]
 
-    def _on_configuration_selecet(self, change):
-        """Update eos preview"""
-        if change["new"]:
-            self._render()
+        # The configuration list is the intersection of two pseudos
+        self.select_configuration.options = [
+            i
+            for i in CONFIGURATIONS
+            if i in _data_ref.keys() and i in _data_comp.keys()
+        ]
 
-    def _render(self):
-        """once called render plot with current instance"""
-        output = self.eos_preview
-        label = self.select_pseudo.value
+        self.update_plot()
+
+    def update_plot(self):
+        """Trigger plot update"""
+        label_ref = self.select_pseudo_ref.value
+        label_comp = self.select_pseudo_comp.value
         configuration = self.select_configuration.value
 
-        data = self.pseudos[label]["accuracy"]["delta"].get(configuration, None)
-        fig, ax = plt.subplots(1, 1, figsize=(440 * _px, 400 * _px))
-        fig.canvas.header_visible = False
-        self._render_plot(ax, data=data, configuration=configuration)
+        data_ref = self.pseudos[label_ref]["accuracy"]["delta"].get(configuration, None)
+        data_comp = self.pseudos[label_comp]["accuracy"]["delta"].get(
+            configuration, None
+        )
 
-        with output:
+        with self.eos_preview:
             clear_output(wait=True)
+            fig = self._render_plot(
+                data_ref, data_comp, configuration, titles=(label_ref, label_comp)
+            )
+            fig.canvas.header_visible = False
             display(fig.canvas)
 
+    def _on_configuration_change(self, change):
+        """Update eos preview"""
+        if change["new"] is not None:
+            self.update_plot()
+
     @staticmethod
-    def _render_plot(ax, data, configuration):
-        """render preview of eos result"""
-        volumes = data["eos"]["output_volume_energy"]["volumes"]
-        energies = data["eos"]["output_volume_energy"]["energies"]
+    def _render_plot(data_ref, data_comp, configuration, titles=("EOS", "EOS")):
+        """render preview of EOS comparison result."""
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(1024 * _px, 440 * _px))
 
-        dense_volume_max = max(volumes)
-        dense_volume_min = min(volumes)
+        # plot to ax1
+        plot_eos(ax1, data_ref, configuration, title=titles[0])
+        # plot to ax2
+        plot_eos(ax2, data_comp, configuration, title=titles[1])
 
-        dense_volumes = np.linspace(dense_volume_min, dense_volume_max, 100)
+        return fig
 
-        E0 = data["eos"]["output_birch_murnaghan_fit"]["energy0"]
-        ref_V0, ref_B0, ref_B01 = data["output_parameters"]["reference_wien2k_V0_B0_B1"]
-        V0, B0, B01 = data["output_parameters"]["birch_murnaghan_results"]
 
-        ae_eos_fit_energy = birch_murnaghan(
-            V=dense_volumes,
-            E0=E0,  # in future update E0 from referece json, where ACWF has E0 stored.
-            V0=ref_V0,
-            B0=ref_B0,
-            B01=ref_B01,
-        )
-        psp_eos_fit_energy = birch_murnaghan(
-            V=dense_volumes,
-            E0=E0,
-            V0=V0,
-            B0=B0,
-            B01=B01,
-        )
+def plot_eos(ax, data, configuration, title="EOS"):
+    """plot EOS result on ax"""
+    volumes = data["eos"]["output_volume_energy"]["volumes"]
+    energies = data["eos"]["output_volume_energy"]["energies"]
 
-        # Plot EOS: this will be done anyway
-        ax.tick_params(axis="y", labelsize=6, rotation=45)
+    dense_volume_max = max(volumes)
+    dense_volume_min = min(volumes)
 
-        ax.plot(volumes, energies, "ob", label="RAW equation of state")
-        ax.plot(dense_volumes, ae_eos_fit_energy, "-b", label="AE reference")
-        ax.axvline(V0, linestyle="--", color="gray")
+    dense_volumes = np.linspace(dense_volume_min, dense_volume_max, 100)
 
-        ax.plot(dense_volumes, psp_eos_fit_energy, "-r", label=f"{configuration} fit")
-        ax.fill_between(
-            dense_volumes,
-            ae_eos_fit_energy,
-            psp_eos_fit_energy,
-            alpha=0.5,
-            color="red",
-        )
+    E0 = data["eos"]["output_birch_murnaghan_fit"]["energy0"]
+    ref_V0, ref_B0, ref_B01 = data["output_parameters"]["reference_wien2k_V0_B0_B1"]
+    V0, B0, B01 = data["output_parameters"]["birch_murnaghan_results"]
 
-        center_x = (max(volumes) + min(volumes)) / 2
-        center_y = (max(energies) + min(energies)) / 2
+    ae_eos_fit_energy = birch_murnaghan(
+        V=dense_volumes,
+        E0=E0,  # in future update E0 from referece json, where ACWF has E0 stored.
+        V0=ref_V0,
+        B0=ref_B0,
+        B01=ref_B01,
+    )
+    psp_eos_fit_energy = birch_murnaghan(
+        V=dense_volumes,
+        E0=E0,
+        V0=V0,
+        B0=B0,
+        B01=B01,
+    )
 
-        # write text of nu value in close middle
-        nu = rel_errors_vec_length(ref_V0, ref_B0, ref_B01, V0, B0, B01)
-        nu = round(nu, 3)
-        delta = round(data["output_parameters"]["delta/natoms"], 3)
-        plt.text(center_x, center_y, f"$\\nu$={nu}\n$\\Delta$={delta} meV/atom")
+    # Plot EOS: this will be done anyway
+    ax.tick_params(axis="y", labelsize=6, rotation=45)
 
-        ax.legend(loc="upper center")
-        ax.set_xlabel("Cell volume per formula unit ($\\AA^3$)", fontsize=8)
-        ax.set_ylabel("$E - TS$ per formula unit (eV)", fontsize=8)
+    ax.plot(volumes, energies, "ob", label="RAW equation of state")
+    ax.plot(dense_volumes, ae_eos_fit_energy, "-b", label="AE reference")
+    ax.axvline(V0, linestyle="--", color="gray")
 
-        ax.get_yaxis().set_ticks([])
+    ax.plot(dense_volumes, psp_eos_fit_energy, "-r", label=f"{configuration} fit")
+    ax.fill_between(
+        dense_volumes,
+        ae_eos_fit_energy,
+        psp_eos_fit_energy,
+        alpha=0.5,
+        color="red",
+    )
+
+    center_x = (max(volumes) + min(volumes)) / 2
+    center_y = (max(energies) + min(energies)) / 2
+
+    # write text of nu value in close middle
+    nu = rel_errors_vec_length(ref_V0, ref_B0, ref_B01, V0, B0, B01)
+    nu = round(nu, 3)
+    delta = round(data["output_parameters"]["delta/natoms"], 3)
+    ax.text(center_x, center_y, f"$\\nu$={nu}\n$\\Delta$={delta} meV/atom")
+
+    ax.legend(loc="upper center")
+    ax.set_xlabel("Cell volume per formula unit ($\\AA^3$)", fontsize=8)
+    ax.set_ylabel("$E - TS$ per formula unit (eV)", fontsize=8)
+    ax.get_yaxis().set_ticks([])
+    ax.set_title(title, fontsize=8)
